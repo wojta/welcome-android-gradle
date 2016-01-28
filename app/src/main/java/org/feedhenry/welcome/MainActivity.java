8dc5530f4b27bf33a3e5ca90e0114aa82888bdcf
Daniel Passos <daniel@passos.me>
2016-01-28 19:40:37 -0200

    Add menu

--- app/src/main/java/org/feedhenry/welcome/MainActivity.java
+++ app/src/main/java/org/feedhenry/welcome/MainActivity.java
@@ -1,13 +1,46 @@
 package org.feedhenry.welcome;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.NavigationView.OnNavigationItemSelectedListener;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+	private DrawerLayout drawerLayout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+
+		final ActionBar actionBar = getSupportActionBar();
+		if (actionBar != null) {
+			actionBar.setHomeAsUpIndicator(R.drawable.ic_drawer);
+			actionBar.setDisplayHomeAsUpEnabled(true);
+		}
+
+		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+		NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+		navigationView.setNavigationItemSelectedListener(new OnNavigationItemSelectedListener() {
+			@Override public boolean onNavigationItemSelected(MenuItem menuItem) {
+				Toast.makeText(getApplicationContext(),
+						menuItem.getTitle() + " pressed", Toast.LENGTH_SHORT).show();
+				menuItem.setChecked(true);
+				drawerLayout.closeDrawers();
+				return true;
+			}
+		});
+
 	}
+
 }
