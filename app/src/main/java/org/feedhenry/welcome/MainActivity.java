4ed01882ce3d9d697a6d97b5123253ce50592846
Daniel Passos <daniel@passos.me>
2016-01-29 17:48:52 -0200

    Add Home and Stats

--- app/src/main/java/org/feedhenry/welcome/MainActivity.java
+++ app/src/main/java/org/feedhenry/welcome/MainActivity.java
@@ -3,14 +3,22 @@ package org.feedhenry.welcome;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.NavigationView.OnNavigationItemSelectedListener;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import org.feedhenry.welcome.fragments.HomeFragment;
+import org.feedhenry.welcome.fragments.StatisticsFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+	private static final String LOG = MainActivity.class.getSimpleName();
 
 	private DrawerLayout drawerLayout;
 
@@ -19,28 +27,57 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		// -- Toolbar
 		final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 
+		// -- Actionbar
 		final ActionBar actionBar = getSupportActionBar();
 		if (actionBar != null) {
 			actionBar.setHomeAsUpIndicator(R.drawable.ic_drawer);
 			actionBar.setDisplayHomeAsUpEnabled(true);
 		}
 
+		// -- Drawer & NavigationView
 		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
 		NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
 		navigationView.setNavigationItemSelectedListener(new OnNavigationItemSelectedListener() {
-			@Override public boolean onNavigationItemSelected(MenuItem menuItem) {
-				Toast.makeText(getApplicationContext(),
-						menuItem.getTitle() + " pressed", Toast.LENGTH_SHORT).show();
-				menuItem.setChecked(true);
+			@Override
+			public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+				Fragment fragment = null;
+
+				switch (menuItem.getItemId()) {
+					case R.id.drawer_home:
+						fragment = new HomeFragment();
+						break;
+					case R.id.drawer_statistics:
+						fragment = new StatisticsFragment();
+						break;
+				}
+
+				if ((fragment != null && (!menuItem.isChecked()))) {
+					FragmentManager fragmentManager = getSupportFragmentManager();
+					fragmentManager.beginTransaction()
+							.replace(R.id.content, fragment)
+							.addToBackStack(null).commit();
+
+					menuItem.setChecked(true);
+				} else {
+					Toast.makeText(getApplicationContext(),
+							"This feature is not implemented yet", Toast.LENGTH_SHORT).show();
+				}
+
 				drawerLayout.closeDrawers();
 				return true;
 			}
 		});
 
+		getSupportFragmentManager()
+				.beginTransaction()
+				.replace(R.id.content, new HomeFragment())
+				.commit();
 	}
 
 }
---
+++ app/src/main/java/org/feedhenry/welcome/fragments/StatisticsFragment.java
@@ -0,0 +1,18 @@
+package org.feedhenry.welcome.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.feedhenry.welcome.R;
+
+public class StatisticsFragment extends android.support.v4.app.Fragment {
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_statistics, null);
+	}
+}
